390e92ca3cb1f7457f5dd948bfdaca19c32382f7
Gaurav Mohabe <gmohabe12@gmail.com>
2023-02-13 14:49:01 +0530

    Added:User name Problem

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 
 namespace User_Ragistration
 {
@@ -7,6 +8,10 @@ namespace User_Ragistration
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To User Registration Program");
+
+            Console.WriteLine("\nEnter User Name:");
+            Regex user = new Regex(User_data.UserName);
+            Console.WriteLine(user.IsMatch(Console.ReadLine()));
         }
     }
 }
---
+++ User_data.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace User_Ragistration
+{
+    internal class User_data
+    {
+        public static string UserName = @"^[A-Z][a-z]{3,}$";
+    }
+}
