233cd133adce23561c935957fa46c9128f74c4e4
Gaurav Mohabe <gmohabe12@gmail.com>
2023-02-15 16:42:59 +0530

    Added: pre-defined password at least one capital numbur

--- Program.cs
+++ Program.cs
@@ -9,26 +9,9 @@ namespace User_Ragistration
         {
             Console.WriteLine("Welcome To User Registration Program");
 
-            Console.WriteLine("\nEnter User Name:");
-            Regex user = new Regex(User_data.UserName);
-            Console.WriteLine(user.IsMatch(Console.ReadLine()));
-
-            Console.WriteLine("User Last Name:");
-            Regex last = new Regex(User_data.UserLastName);
-            Console.WriteLine(last.IsMatch(Console.ReadLine()));
-
-            Console.WriteLine("User Enter valid E-mail:");
-            Regex emails = new Regex(User_data.Email);
-            Console.WriteLine(emails.IsMatch(Console.ReadLine()));
-
-            Console.WriteLine("Enter Your mobile numbur(+91):");
-            Regex mob = new Regex(User_data.MobNumValidation);
-            Console.WriteLine(mob.IsMatch(Console.ReadLine()));
-
-
-            Console.WriteLine("Enter Your Pasward..:\nplease Enter at least minimum 8 Character..");
-            Regex pass = new Regex(User_data.User_password);
-            Console.WriteLine(pass.IsMatch(Console.ReadLine())); 
+           User_data data= new User_data();
+            data.Datacheck();
+           
 
 
         }
--- User_data.cs
+++ User_data.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace User_Ragistration
 {
@@ -10,7 +11,29 @@ namespace User_Ragistration
         public static string UserLastName = @"^[A-Z][a-z]{3,}$";
         public static string Email = @"^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+$";
         public static string MobNumValidation = @"^[+]91[0-9]{10}$";
-        public static string User_password = @"^[a-z]{9,}$";
+        public static string User_password = @"^(?=.*[A-Z]).{8,}$";
 
+        public void Datacheck()
+        {
+            Console.WriteLine("\nEnter User Name:");
+            Regex user = new Regex(User_data.UserName);
+            Console.WriteLine(user.IsMatch(Console.ReadLine()));
+
+            Console.WriteLine("User Last Name:");
+            Regex last = new Regex(User_data.UserLastName);
+            Console.WriteLine(last.IsMatch(Console.ReadLine()));
+
+            Console.WriteLine("User Enter valid E-mail:");
+            Regex emails = new Regex(User_data.Email);
+            Console.WriteLine(emails.IsMatch(Console.ReadLine()));
+
+            Console.WriteLine("Enter Your mobile numbur(+91):");
+            Regex mob = new Regex(User_data.MobNumValidation);
+            Console.WriteLine(mob.IsMatch(Console.ReadLine()));
+
+            Console.WriteLine("Enter Your Pasward..:\nplease Enter at least 1 Uppercase\nand minimum 8 Character..");
+            Regex pass = new Regex(User_data.User_password);
+            Console.WriteLine(pass.IsMatch(Console.ReadLine()));
+        }
     }
 }
